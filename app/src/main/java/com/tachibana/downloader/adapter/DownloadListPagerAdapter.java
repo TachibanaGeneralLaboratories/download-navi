889bd27e6a7baf3c8ba5320ccd91e241b98f2a3b
proninyaroslav <proninyaroslav@gmail.com>
2019-01-06 14:12:26 +0300

    Base UI layout

--- app/src/main/java/com/tachibana/downloader/adapter/DownloadListPagerAdapter.java
+++ app/src/main/java/com/tachibana/downloader/adapter/DownloadListPagerAdapter.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2019 Tachibana General Laboratories, LLC
+ * Copyright (C) 2019 Yaroslav Pronin <proninyaroslav@mail.ru>
+ *
+ * This file is part of Download Navi.
+ *
+ * Download Navi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Download Navi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Download Navi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.tachibana.downloader.adapter;
 
 import android.content.Context;
@@ -26,6 +46,7 @@ public class DownloadListPagerAdapter extends FragmentPagerAdapter
     @Override
     public Fragment getItem(int position)
     {
+        /* Stubs */
         switch (position) {
             case QUEUED_FRAG_POS:
                 return new Fragment();
