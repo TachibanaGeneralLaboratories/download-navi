081356521003bc3e4bb2c0dd5066cfbb6fbf735b
proninyaroslav <proninyaroslav@gmail.com>
2019-01-30 22:13:42 +0300

    Fix license notice

--- app/src/main/java/com/tachibana/downloader/core/exception/FreeSpaceException.java
+++ app/src/main/java/com/tachibana/downloader/core/exception/FreeSpaceException.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2019 Tachibana General Laboratories, LLC
+ * Copyright (C) 2019 Yaroslav Pronin <proninyaroslav@mail.ru>
+ *
+ * This file is part of Download Navi.
+ *
+ * Download Navi is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Download Navi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Download Navi.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.tachibana.downloader.core.exception;
 
 /*
