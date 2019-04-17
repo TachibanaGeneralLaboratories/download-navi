bffa3a2f87e6aca5d065d06e7b2b3830144ed823
proninyaroslav <proninyaroslav@gmail.com>
2019-04-17 19:14:20 +0300

    Fix notification's "Couldn't expand RemoteViews" error in Android 4.4
    Replace the vector drawable to PNG.

--- app/src/main/java/com/tachibana/downloader/service/DownloadService.java
+++ app/src/main/java/com/tachibana/downloader/service/DownloadService.java
@@ -314,7 +314,7 @@ public class DownloadService extends LifecycleService
                         PendingIntent.FLAG_UPDATE_CURRENT);
 
         return new NotificationCompat.Action.Builder(
-                R.drawable.ic_power_settings_new_menu_24dp,
+                R.drawable.ic_power_white_24dp,
                 getString(R.string.shutdown),
                 shutdownPendingIntent)
                 .build();
