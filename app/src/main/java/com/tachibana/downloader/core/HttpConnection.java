5dab44d1140be0da11d92faa6e0b743d82ed3e7f
Yaroslav Pronin <proninyaroslav@gmail.com>
2020-09-03 21:35:43 +0300

    Add missing import

--- app/src/main/java/com/tachibana/downloader/core/HttpConnection.java
+++ app/src/main/java/com/tachibana/downloader/core/HttpConnection.java
@@ -25,6 +25,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.GeneralSecurityException;
+import android.webkit.CookieManager;
 
 import javax.net.ssl.HttpsURLConnection;
 
