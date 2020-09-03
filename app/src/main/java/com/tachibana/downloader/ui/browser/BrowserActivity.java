c5926b3ba0663c58f89187ee3230b0bdd6c67bff
abelfn <70645835+abelfn@users.noreply.github.com>
2020-09-03 11:46:46 -0700

    revert

--- app/src/main/java/com/tachibana/downloader/ui/browser/BrowserActivity.java
+++ app/src/main/java/com/tachibana/downloader/ui/browser/BrowserActivity.java
@@ -435,11 +435,6 @@ public class BrowserActivity extends AppCompatActivity
                 webView.reload();
                 break;
             case R.id.save_page_menu:
-                createFolderSub();
-                File root = Environment.getExternalStorageDirectory();
-                File file = new File(root.getAbsolutePath() + "/"+Environment.DIRECTORY_DOWNLOADS + "/offline" + "/"+webView.getTitle());
-                webView.saveWebArchive(file + ".mht");
-                Toast.makeText(BrowserActivity.this, "Page saved!", Toast.LENGTH_SHORT).show();
                 break;    
             case R.id.bookmarks_menu:
                 showBookmarks();
@@ -460,18 +455,7 @@ public class BrowserActivity extends AppCompatActivity
         return true;
     }
 
-       public void createFolderSub(){
-        String state;
-        state = Environment.getExternalStorageState();
-        if ((Environment.MEDIA_MOUNTED).equals(state)) {
-            File root = Environment.getExternalStorageDirectory();
-            File Dir = new File(root.getAbsolutePath() + "/"+Environment.DIRECTORY_DOWNLOADS + "/offline" );
-            if (!Dir.exists()) {
-                Dir.mkdir();
-            }
-        }
-    }
-
+   
     private void makeShareDialog(String url)
     {
         if (url == null)
